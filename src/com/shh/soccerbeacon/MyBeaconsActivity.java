b674fd3c30b961ea307bc414979be5cec2e9d4e1
Suk Hwan Hong <suk.hong@sparity.com>
2015-10-25 01:42:58 -0400

    New Activity

--- src/com/shh/soccerbeacon/MyBeaconsActivity.java
+++ src/com/shh/soccerbeacon/MyBeaconsActivity.java
@@ -1,23 +1,22 @@
 package com.shh.soccerbeacon;
 
 import android.support.v7.app.ActionBarActivity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends ActionBarActivity {
+public class MyBeaconsActivity extends ActionBarActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_mybeacons);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
+		getMenuInflater().inflate(R.menu.mybeacons, menu);
 		return true;
 	}
 
@@ -27,11 +26,7 @@ public class MainActivity extends ActionBarActivity {
 		// automatically handle clicks on the Home/Up button, so long
 		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
-		if (id == R.id.action_mybeacons) 
-		{
-			Intent intent = new Intent(this, MyBeaconsActivity.class);
-			startActivity(intent);
-			    
+		if (id == R.id.action_settings) {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
