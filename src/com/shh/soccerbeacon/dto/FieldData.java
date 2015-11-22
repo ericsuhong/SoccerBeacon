ad5523b545ea83eb04e7ab8d028b41296e468e2d
Suk Hwan Hong <suk.hong@gatech.edu>
2015-11-22 02:57:21 -0500

    test

--- src/com/shh/soccerbeacon/dto/FieldData.java
+++ src/com/shh/soccerbeacon/dto/FieldData.java
@@ -2,7 +2,7 @@ package com.shh.soccerbeacon.dto;
 
 import java.util.ArrayList;
 
-// temporary store for field data during lifecycle of an application
+// temporary store for field data during life cycle of an application
 public class FieldData
 {
 	public static int field_width;
